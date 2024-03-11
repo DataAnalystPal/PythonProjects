a7e70d7f161be7b62076ee60dabe7834d45bb7c0
Rahul Pal <115947068+DataAnalystPal@users.noreply.github.com>
2024-03-11 21:24:31 +0530

    Add files via upload

---
+++ 01_RandomisationAndLists/01_RockPaperScissors.py
@@ -0,0 +1,62 @@
+# Project: The Game of Rock, Paper and Scissors
+# You against the Computer
+# Rules of the game at: wrpsa.com
+# Concepts Utilised: Random Module, List, Control Flow (if-else statements)
+
+#import random module
+import random
+
+#create variables
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#create a list
+game_images = [rock, paper, scissors]
+
+#Main Code
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+if user_choice >= 3 or user_choice < 0: 
+    print("You typed an invalid number, you lose!") 
+else:
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0, 2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
+
